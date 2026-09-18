3d801c280d1c36d9b1fa11435611095f96305af1
Adarsh_Jaiswal_57 <adarshkumr1657@gmail.com>
2026-09-19 00:11:51 +0530

    Time: 0 ms (100%), Space: 9.9 MB (89.44%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,24 +1,65 @@
+// class Solution {
+// public:
+//     bool checkInclusion(string s1, string s2) {
+        
+//         if (s1.size() > s2.size())
+//             return false;
+
+//         sort(s1.begin(), s1.end());
+
+//         int k = s1.size();
+
+//         for (int i = 0; i <= s2.size() - k; i++) {
+            
+//             string temp = s2.substr(i, k);
+
+//             sort(temp.begin(), temp.end());
+
+//             if (temp == s1)
+//                 return true;
+//         }
+
+//         return false;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        
-        if (s1.size() > s2.size())
-            return false;
-
-        sort(s1.begin(), s1.end());
-
-        int k = s1.size();
-
-        for (int i = 0; i <= s2.size() - k; i++) {
-            
-            string temp = s2.substr(i, k);
-
-            sort(temp.begin(), temp.end());
-
-            if (temp == s1)
-                return true;
+        if(s1.size() > s2.size()) return false;
+        vector<int> freq1(26,0),freq2(26,0);
+        for(auto it : s1) {
+            freq1[it-'a']++;
         }
-
+        int k = s1.size();
+        for(int i =0;i<k;i++){
+            freq2[s2[i]-'a']++;
+        }
+        if(freq1 == freq2) return true;
+        for(int i =k;i<s2.size();i++){
+            freq2[s2[i] -'a']++;
+            freq2[s2[i - k] - 'a']--;
+            if(freq1 == freq2) return true;
+        }
         return false;
     }
 };
